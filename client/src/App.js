4b9d071bbbef203691878734daa6a6a6f21ff9ef
Daniel Levenson <daniellevenson4@gmail.com>
2017-11-18 14:41:07 -0500

    set up nav components, rendering nav and header through app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import SocialMedia from './components/SocialMedia'
+import Nav from './components/Nav'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <SocialMedia />
+        <Nav />
         <Header />
         <Footer />
       </div>
---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Nav = () => {
+	return(
+		<div className="nav">
+			<nav>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Reservations</a></li>
+					<li><a href="#">Menu</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Contact </a></li>
+				</ul>
+			</nav>
+		</div>
+		)
+}
+
+export default Nav
