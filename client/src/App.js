15fa326815f8472e247e11d906716e22451872ab
Daniel Levenson <daniellevenson4@gmail.com>
2017-11-18 14:28:55 -0500

    created public directory within components, added links and images for social media pages, rendered through App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import SocialMedia from './components/SocialMedia'
 import Footer from './components/Footer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <SocialMedia />
+
         <Footer />
       </div>
     );
