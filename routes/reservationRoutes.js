ddde72189843e71c46ab27951691bcf560fc90c8
Ileana Ordonez <ileana.s.ordonez@gmail.com>
2017-11-27 17:27:22 -0500

    added tables route

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const reservationController = require('../controllers/reservationController');
+const mesasController = require('../controllers/mesasController');
 
 const reservationRoutes = express.Router()
 
 reservationRoutes.get('/',reservationController.index);
+reservationRoutes.get('/mesas',mesasController.index);
 reservationRoutes.get('/:id', reservationController.show);
 reservationRoutes.get('/email/:email', reservationController.find);
 reservationRoutes.post('/', reservationController.create);
 reservationRoutes.put('/:id', reservationController.update);
 reservationRoutes.delete('/:id', reservationController.destroy);
+
+
 
 module.exports = reservationRoutes;
