f09517a9201e1358b264ca36a59ec8ca177803b2
Daniel Levenson <daniellevenson4@gmail.com>
2017-11-28 15:24:02 -0500

    added about content

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 function About(){
   return(
     <div className='about'>
-      <p>about section here and hereeee</p>
+    	<h1 className="about-header">About Us...</h1>
+    	<p className="about-descr">
+    	La Bamba Mofongo is the premier spot in New York City's Flatiron district.  With food prepared by Head Chef Susie, we offer an array of fine hispanic cuisine.  To go with our fine cuisine we also serve drinks until 3am, so while your meal may be ending, your night is just beginning.
+    	</p>
     </div>
   )
 }
