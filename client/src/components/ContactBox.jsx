cd637d269bcda85db64134bfcdb8f8088bd998a8
Daniel Levenson <daniellevenson4@gmail.com>
2017-11-28 14:45:56 -0500

    final styling for contact component

--- client/src/components/ContactBox.jsx
+++ client/src/components/ContactBox.jsx
@@ -4,8 +4,8 @@ function ContactBox(){
   return(
     <div className='contactBox'>
         <h1 className="contact-header">Contact</h1>
-      	<p className="email">Email:   <a href="mailto:labamba@mofongo.com">Labamab@mofongo.com</a></p>
-      	<p className="phone">Phone:   <a href="tel:212-555-1234">212-555-1234</a></p>
+      	<p className="contact-info">Email:   <a href="mailto:labamba@mofongo.com">Labamab@mofongo.com</a></p>
+      	<p className="contact-info">Phone:   <a href="tel:212-555-1234">212-555-1234</a></p>
     </div>
   )
 }
--- client/src/components/HoursBox.jsx
+++ client/src/components/HoursBox.jsx
@@ -4,12 +4,12 @@ function HoursBox(){
   return(
     <div className='hoursBox'>
       <h1 className="hours-header">Hours of Operation</h1>
-      <h3 className="hours-subh">Kitchen Hours</h3>
-      <p className="kitchen-hours">Daily</p>
-      <p className="kitchen-hours">12p - 12a</p>
-      <h3 className="hours-subh">Bar Hours</h3>
-      <p className="bar-hours">Daily</p>
-      <p className="bar-hours">12p - 4a</p>
+      <h3 className="hours-header">Kitchen Hours</h3>
+      <p className="hours">Daily</p>
+      <p className="hours">12p - 12a</p>
+      <h3 className="hours-header">Bar Hours</h3>
+      <p className="hours">Daily</p>
+      <p className="hours">12p - 4a</p>
     </div>
   )
 }
