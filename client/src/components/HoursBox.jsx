75d46a16266c5416580637748d1bb062e2d71eaf
Daniel Levenson <daniellevenson4@gmail.com>
2017-11-28 15:38:19 -0500

    manually merged in contact styling

--- client/src/components/HoursBox.jsx
+++ client/src/components/HoursBox.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 function HoursBox(){
   return(
     <div className='hoursBox'>
-      hours section here
+      <h1 className="hours-header">Hours of Operation</h1>
+      <h3 className="hours-header">Kitchen Hours</h3>
+      <p className="hours">Daily</p>
+      <p className="hours">12p - 12a</p>
+      <h3 className="hours-header">Bar Hours</h3>
+      <p className="hours">Daily</p>
+      <p className="hours">12p - 4a</p>
     </div>
   )
 }
