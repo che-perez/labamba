26ebeffaba771067525ecd869cf9fb5e19dd3c77
Daniel Levenson <daniellevenson4@gmail.com>
2017-11-28 15:50:36 -0500

    styling for about component

--- client/src/components/ContactBox.jsx
+++ client/src/components/ContactBox.jsx
@@ -4,8 +4,8 @@ function ContactBox(){
   return(
     <div className='contactBox'>
         <h1 className="contact-header">Contact</h1>
-      	<p className="contact-info">Email:   <a href="mailto:labamba@mofongo.com">Labamab@mofongo.com</a></p>
-      	<p className="contact-info">Phone:   <a href="tel:212-555-1234">212-555-1234</a></p>
+      	<p className="contact-info">Email:   <a href="mailto:labamba@mofongo.com">Labamaba@mofongo.com</a></p>
+      	<p className="contact-info">Phone:   <a href="tel:212-555-1212">212-555-1212</a></p>
     </div>
   )
 }
