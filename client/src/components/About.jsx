f8aac10369dfb1b56117b426d91c6304782dc1e3
Daniel Levenson <daniellevenson4@gmail.com>
2017-11-28 19:01:57 -0500

    changed about component to flex styling

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -5,7 +5,7 @@ function About(){
     <div className='about'>
     	<h1 className="about-header">About Us...</h1>
     	<p className="about-descr">
-    	La Bamba Mofongo is the premier spot in New York City's Flatiron district.  With food prepared by Head Chef Susie, we offer an array of fine hispanic cuisine.  To go with our fine cuisine we also serve drinks until 3am, so while your meal may be ending, your night is just beginning.
+    	La Bamba Mofongo is the premier spot in New York City's Flatiron district.  With food prepared by Head Chef Susie, we offer an array of fine hispanic cuisine.  To go with our fine cuisine we also serve drinks until 4am, so while your meal may be ending, your night is just beginning.
     	</p>
     </div>
   )
